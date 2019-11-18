b05675f43b4830479ff62347ca6105ede619f189
Estefania <estefaniac.25@hotmail.com>
2019-11-18 01:04:58 -0300

    Se crea formulario

--- Server.js
+++ Server.js
@@ -4,6 +4,34 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+var misFs = require('./public/misfunciones');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/ContactosAlberdi',{ useNewUrlParser: true })
+
+var db = mongoose.connection;
+db.once('open', function(){
+    console.log("Base de datos conectada.");
+});
+
+
+
+/*--------------------- Esquema para la base de datos------------------- */
+var ContactoSchema = mongoose.Schema({
+    nombre: String,
+    apellido: String,
+    correo: String,
+    telefono: Number,
+    mensaje: String,
+});
+
+var Contacto = mongoose.model('Contacto',ContactoSchema);
+
+
+
+//import body-parser
+var bodyParser = require('body-parser');
+
+//creamos un servidor que escucha en el puerto 7070 y envia un mensaje
 var server  = app.listen(7070,function(){
 	var host = server.address().address;
 	var port = server.address().port;
@@ -18,37 +46,44 @@ app.get("/",function(req, res){
 	res.render("index");
 
 });
+/* ----------permitimos a express acceder a nuestro archivo (Contacto.html)--------------------- */
+app.get('/Contacto.html', function(req, res) {
+
+        res.sendFile(__dirname + "/" + "Contacto.html");
+        console.log(__dirname + "/" + "Contacto.html")
+        var respuesta = misFs.cabecera();
+
+    });
 
 
-var miVector = [];
-app.post('/enviarInfo',function(req,res){
-	console.log("/enviarInfo - POST");
-	console.log(req.body.apellido);
-	
-	miVector.push(req.body.apellido);
-	miVector.push(req.body.nombre);
-	miVector.push(req.body.Correo);
-	miVector.push(req.body.telefono);
-	res.send("<h1>se envio con exito el formulario</h1>")
+app.post('/carga',function(req,res){
+    console.log("--(post/carga)-->[server]");
+
+    /*Instancio un objeto documento de la clase Cliente*/
+    var contacto = new Contacto({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        correo: req.body.correo,
+        telefono: req.body.telefono,
+        mensaje: req.body.mensaje,
+    });
+    /*Insertar cliente en la colección*/
+    contacto.save(function(error, documento){
+        console.log("--(cliente)-->[ BBDD ]");
+        if(error){
+            console.log("<--error--[ BBDD ]");
+        }else{
+            console.log(documento);
+            console.log("<-- OK --[ BBDD ]");
+        };
+    });
+    console.log("<--( <a index.html>) --[ server ]");
+
+    res.send("<p>Cargado</p><a href='/Contacto.html' >Volver</a>");
+
 });
 
-
-
-
- 
-app.get('/devolverTabla',function(req,res){
-	res.send(dameTabla(miVector));
-});
-function dameTabla(Vect){
-	var str ="";
-	var i=0;
-	for(i=0;i<Vect.length;i++){
-		str =str +"<td>"+Vect[i]+"</td>";
-	};
-	str = "<table border = 1><tr>"+str+"</tr></table>"
-	console.log(str);
-	return str;
-}
+/*---------------------------------FIN FORMULARIO-----------------------------*/
 
 var usuarios = [];
 app.post('/enviarDatos',function(req,res){
