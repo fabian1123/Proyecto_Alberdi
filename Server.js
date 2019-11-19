7cbaaa2ef4a6ed3bed03ff43f88cd3c1ebfe9fcd
Estefania <estefaniac.25@hotmail.com>
2019-11-19 17:43:20 -0300

    Agrega login.html

--- Server.js
+++ Server.js
@@ -1,112 +1,162 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+        var express = require('express');
+        var app = express();
+        var bodyParser = require("body-parser");
+        app.use(bodyParser.urlencoded({extended:false}));
+        app.use(bodyParser.json());
 
-var misFs = require('./public/misfunciones');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ContactosAlberdi',{ useNewUrlParser: true })
+        var misFs = require('./public/misfunciones');
+        var mongoose = require('mongoose');
+        mongoose.connect('mongodb://localhost/ContactosAlberdi',{ useNewUrlParser: true })
 
-var db = mongoose.connection;
-db.once('open', function(){
-    console.log("Base de datos conectada.");
-});
-
-
-
-/*--------------------- Esquema para la base de datos------------------- */
-var ContactoSchema = mongoose.Schema({
-    nombre: String,
-    apellido: String,
-    correo: String,
-    telefono: Number,
-    mensaje: String,
-});
-
-var Contacto = mongoose.model('Contacto',ContactoSchema);
+        var db = mongoose.connection;
+        db.once('open', function(){
+            console.log("Base de datos conectada.");
+        });
 
 
-
-//import body-parser
-var bodyParser = require('body-parser');
-
-//creamos un servidor que escucha en el puerto 7070 y envia un mensaje
-var server  = app.listen(7070,function(){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log("Example app listening at htpp://%s:%s",host, port)
-});
-
-app.use(express.static(__dirname + "/public"));
-
-//routes
-
-app.get("/",function(req, res){
-	res.render("index");
-
-});
-/* ----------permitimos a express acceder a nuestro archivo (Contacto.html)--------------------- */
-app.get('/Contacto.html', function(req, res) {
-
-        res.sendFile(__dirname + "/" + "Contacto.html");
-        console.log(__dirname + "/" + "Contacto.html")
-        var respuesta = misFs.cabecera();
-
-    });
+        /*--------------------- Esquema de las Collectiones de la base de datos------------------- */
+        var ContactoSchema = mongoose.Schema({
+            nombre: String,
+            apellido: String,
+            correo: String,
+            telefono: Number,
+            mensaje: String,
+        });
+        var UsuarioSchema = mongoose.Schema({
+            usuario: String,
+            contraseña: String,
+         });
+        var Contacto = mongoose.model('Contacto',ContactoSchema);
+        var Usuario = mongoose.model('Usuario',UsuarioSchema);
 
 
-app.post('/carga',function(req,res){
-    console.log("--(post/carga)-->[server]");
+        /*----------------------creamos un servidor que escucha en el puerto 7070 y envia un mensaje-----*/
+        var server  = app.listen(7070,function(){
+            var host = server.address().address;
+            var port = server.address().port;
+            console.log("Example app listening at htpp://%s:%s",host, port)
+        });
 
-    /*Instancio un objeto documento de la clase Cliente*/
-    var contacto = new Contacto({
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        correo: req.body.correo,
-        telefono: req.body.telefono,
-        mensaje: req.body.mensaje,
-    });
-    /*Insertar cliente en la colección*/
-    contacto.save(function(error, documento){
-        console.log("--(cliente)-->[ BBDD ]");
-        if(error){
-            console.log("<--error--[ BBDD ]");
-        }else{
-            console.log(documento);
-            console.log("<-- OK --[ BBDD ]");
-        };
-    });
-    console.log("<--( <a index.html>) --[ server ]");
+        app.use(express.static(__dirname + "/public"));
 
-    res.send("<p>Cargado</p><a href='/Contacto.html' >Volver</a>");
+        //routes
 
-});
+        app.get("/",function(req, res){
+            res.render("index");
 
-/*---------------------------------FIN FORMULARIO-----------------------------*/
+        });
+        /* ----------permitimos a express acceder a nuestro archivo (Contacto.html)--------------------- */
+        app.get('/Contacto.html', function(req, res) {
 
-var usuarios = [];
-app.post('/enviarDatos',function(req,res){
-	console.log("/enviarDatos - POST");
-	console.log(req.body.usuario);
-	console.log(req.body.contraseña);
-	
-	usuarios.push(req.body.usuario);
-	usuarios.push(req.body.contraseña);
-	res.send('se ha logueado con exito')
-});
+                res.sendFile(__dirname + "/" + "Contacto.html");
+                console.log(__dirname + "/" + "Contacto.html")
 
-app.get('/devolverDatos',function(req,res){
-	res.send(dameTabla2(usuarios));
-});
+            });
+        app.get('/Login.html', function(req,res){
+                res.sendFile(__dirname + "/Login.hrml");
+                console.log(__dirname + "/Login.html")
 
-function dameTabla2(Vect2){
-	var str2 ="";
-	var i=0;
-	for(i=0;i<Vect2.length;i++){
-		str2 =str2 +"<td>"+Vect2[i]+"</td>";
-	};
-	str2 = "<table border = 1><tr>"+str2+"</tr></table>"
-	console.log(str2);
-	return str2;
-}
+        });
+        app.get('/accionesLogin.html', function(req, res) {
+            console.log("---.get('/accionesLogin.html-->[server]");
+
+                res.sendFile(__dirname + "/accionesLogin.html");
+
+        });
+
+
+
+           app.post('/carga',function(req,res,next){
+                        console.log("--(post/carga)-->[server]");
+    //-----Instancio un objeto documento de la clase Cliente
+                var contacto = new Contacto({
+                    nombre: req.body.nombre,
+                    apellido: req.body.apellido,
+                    correo: req.body.correo,
+                    telefono: req.body.telefono,
+                    mensaje: req.body.mensaje,
+                });
+        //-Inserta en la colección
+           // cliente
+                contacto.save(function(error, documento){
+                    console.log("--(cliente)-->[ BBDD ]");
+                    if(error){
+                        console.log("<--error--[ BBDD ]");
+                    }else{
+                        console.log(documento);
+                        console.log("<-- OK --[ BBDD ]");
+                    };
+                });
+                console.log("<--( <a index.html>) --[ server ]");
+
+                res.send("<h2><center>Datos cargados exitosamente</center></h1><a href='/Contacto.html' ><center>Volver</center></a>");
+
+            });
+               /*--------------------Carga los datos por 1 vez--------------*/
+              app.post('/cargaLogin',function(req,res){
+                            console.log("--(post/carga)-->[server]");
+                  var users = new Usuario({
+                                   usuario: req.body.usuario,
+                                   contraseña: req.body.contraseña,
+                               });
+              //usuarios
+                         users.save(function(error, documento){
+                               console.log("--(cliente)-->[ BBDD ]");
+                               if(error){
+                                  console.log("<--error--[ BBDD ]");
+                                }else{
+                                console.log(documento);
+                               console.log("<-- OK --[ BBDD ]");
+                                };
+                             });
+                             console.log("<--( <a index.html>) --[ server ]");
+                             res.send("<h2><center>Usted se há logueado exitosamente!</center></h1><a href='/Login.html'><center>Volver</center></a>");
+
+                   });
+
+
+        app.post("/validar", function (req,res,next){
+
+          Usuario.findOne({ usuario: req.body.usuario, contraseña: req.body.contraseña },
+          function(err, usuarioEncontrado){
+            if (err) throw err;
+
+
+            if(usuarioEncontrado) {
+              //usuario existe
+              res.send("<h2> Usuario validado correctamente </h2>" + usuarioEncontrado +"<p><a href='/validacion.html'>Acciones de Usuario</a></p>");
+            } else {
+              //usuario no existe
+              res.send("<h2> El usuario no existe </h2>");
+            }
+        }).catch(error => res.send({error}));
+
+   });
+   /*-----------------Busca los datos de los Contactos----------------*/
+       app.post('/user', function(req, res){
+               /* Recuperar registros */
+               Contacto.find({}, function(error,documento){
+                   if(error){
+                       console.log("<-- error --[ BBDD ]");
+                   }else{
+                       console.log("<-- (clientes)--[ BBDD ]");
+                       console.log("Datos obtenidos");
+                       console.log(documento.length);
+                       valores=[];
+                       for(var i=0;i<documento.length;i++){
+                            valores[i]=documento[i].nombre+
+                                   " "+documento[i].apellido+
+                                   " "+documento[i].correo+
+                                   " "+documento[i].telefono;
+                       };
+                           strMio=misFs.muestraDatos(valores);
+                           mostrar="<table border=1>"+strMio+"</table>";
+                           res.send("<h2>Listado de clientes</h2>" + mostrar +"<a href='/accionesLogin.html' >Volver</a>");
+
+
+                   }
+               });
+
+           });
+    /*---------------------------------FIN FORMULARIO-----------------------------*/
+
--- public/misfunciones.js
+++ public/misfunciones.js
@@ -1,64 +1,16 @@
 /*  Este archivo contiene mis funciones
 	de uso frecuente                     */
 
-exports.cabecera = function (){
-    return "<h1>Formulario de contacto</h1><h2>Ingrese sus datos y en breve nos pondremos en contacto con usted.</h2>"
-};
-
- exports.dameFila = function(vector){
-    console.log("-- (vector)-->[dameFila]");
-    retorno="";
-    var i=0;
-    for(i=0;i<vector.length;i++){
-        retorno = retorno +"<td>"+vector[i]+"</td>";
-    };
-    console.log("<-- retorno-- [dameFila]");
-    return retorno;
-}
-
- exports.dameColu = function(vector){
-    console.log("-- (vector)-->[dameColu]");
+ exports.muestraDatos = function(vector){
+    console.log("-- (vector)-->[muestraDatos]");
     retorno="";
     var i=0;
     for(i=0;i<vector.length;i++){
         retorno = retorno +"<tr><td>"+vector[i]+"</td></tr>";
     };
-    console.log("<-- retorno-- [dameColu]");
+    console.log("<-- retorno-- [muestraDatos]");
     return retorno;
 }
 
-exports.dameSelector = function(vector_Id,vector){
-    console.log("-- dameSelector(vector)-->[misfunciones]");
-    console.log("<----- (radio+tabla)------[misfunciones]");
-    retorno="";
-    var i=0;
-    for(i=0;i<vector.length;i++){
-        retorno = retorno +
-        "<tr><td><input type='radio' name='opcion' value='"+
-        vector_Id[i]+"'></td><td>"+vector[i]+"</td></tr>";
-    };
-
-    return retorno;
-}
-
-
-exports.dameBoton = function(accion,metodo,control){
-    console.log("-- dameBoton( , , )--> [misfunciones]");
-    console.log("<--- <form>...</form>- [misfunciones]");
-
-    return"<form action='/"+accion+"' method='"+metodo+"'>"+
-        control+
-        "<input type='submit' value='Guardar datos'>"+
-        "</form>";
-}
-
-exports.entradaDatos = function(){
-    console.log("-- entradaDatos() -->[misfunciones]");
-    console.log("<--<input type=text--[misfunciones]");
-    return '<p>Nombre: <input type="text" name="nombre"></p>'+
-    '<p>Apellido: <input type="text" name="apellido"> </p>'+
-    '<p> Correo electronico: <input type="text" name="correo"> </p>' +
-    '<p> Teléfono: <input type="number" name="telefono"> </p>';
-};
 
 
